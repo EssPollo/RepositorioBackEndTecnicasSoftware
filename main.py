e596b06f78b076d49bb533dcbe595d486997c3b3
rnalan <114028779+EssPollo@users.noreply.github.com>
2024-03-15 15:23:45 -0600

    cambio de carpetas

--- main.py
+++ main.py
@@ -5,11 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def root():
-<<<<<<< HEAD
-    return 'hello serch1'
-=======
     return 'hello serch'
->>>>>>> c98cc9a0a47eddacdd151b08b56c7f97c5ef986c
 
 
 if __name__ == '__main__':
